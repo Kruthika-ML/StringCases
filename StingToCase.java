c1bcfd742daae55b394e3ddfef7f422844583fba
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-21 11:07:24 +0530

    Add files via upload

---
+++ StingToCase.java
@@ -0,0 +1,7 @@
+public class StingToCase {
+    public static void main(String[] args) {
+        String txt = "Hello World";
+        System.out.println("To Upper case "+txt.toUpperCase());
+        System.out.println("To Lower case "+txt.toLowerCase());
+    }
+}
